02a2f37a416f14a12357391363b0a99e2e4a46b2
Hanturtle <tjfdnjs0829@naver.com>
2021-06-23 23:40:00 +0900

    setting(server):Spring Boot 초기설정

---
+++ server/src/main/java/com/wisetdev/taggedaddress/server/TaggedAddressServerApplication.java
@@ -0,0 +1,13 @@
+package com.wisetdev.taggedaddress.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaggedAddressServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaggedAddressServerApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/com/wisetdev/taggedaddress/server/TaggedAddressServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wisetdev.taggedaddress.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaggedAddressServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
